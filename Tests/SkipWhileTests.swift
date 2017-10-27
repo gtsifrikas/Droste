9123b6772dd12736068a42d8a75bc8fdd9b198a7
George Tsifrikas <tsifrikas@workable.com>
2017-10-27 18:44:37 +0300

    Create skipWhile operator tests.

---
+++ Tests/SkipWhileTests.swift
@@ -0,0 +1,75 @@
+//
+//  SkipWhileTests.swift
+//  RxCache
+//
+//  Created by George Tsifrikas on 27/10/2017.
+//
+
+import Foundation
+import Foundation
+import Nimble
+import Quick
+import RxSwift
+import RxTest
+import RxCache
+
+class SkipWhileTests: QuickSpec {
+    override func spec() {
+        describe("The skipWhile operator") {
+            var scheduler: TestScheduler!
+            var cacheObserver: TestableObserver<Int?>!
+            var cache: CacheFake<String, Int>!
+            var cacheUnderTest: CompositeCache<String, Int>!
+            let key = "testKey"
+            
+            var aCondition: Bool = false
+            
+            beforeEach {
+                cache = CacheFake<String, Int>()
+                scheduler = TestScheduler(initialClock: 0)
+                cacheObserver = scheduler.createObserver(Int?.self)
+                
+                cacheUnderTest = cache.skipWhile({ (key) -> Observable<Bool> in
+                    return Observable.just(aCondition)
+                })
+            }
+            
+            context("When condition is false") {
+                beforeEach {
+                    aCondition = false
+                    
+                    scheduler.scheduleAt(0) {
+                        _ = cacheUnderTest.get(key).subscribe(cacheObserver)
+                    }
+                    
+                    scheduler.scheduleAt(10) {
+                        cache.request.on(.next(1))
+                    }
+                    
+                    scheduler.start()
+                }
+                
+                it("should not skip cache") {
+                    expect(cacheObserver.events).toEventually(haveCount(1))
+                    expect(cache.numberOfTimesCalledGet).toEventually(equal(1))
+                }
+            }
+            
+            context("When condition is true") {
+                beforeEach {
+                    aCondition = true
+                    
+                    scheduler.scheduleAt(0) {
+                        _ = cacheUnderTest.get(key).subscribe(cacheObserver)
+                    }
+                    
+                    scheduler.start()
+                }
+                
+                it("should skip asking cache for the value") {
+                    expect(cache.numberOfTimesCalledGet).toEventually(equal(0))
+                }
+            }
+        }
+    }
+}
