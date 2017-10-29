f6235e618f26ceb276af199d4e966a6d5c898c84
George Tsifrikas <tsifrikas@workable.com>
2017-10-29 23:59:05 +0200

    Create custom implementation of session.dataTask handling in NetworkFetcher.

--- Sources/Fetchers/NetworkFetcher.swift
+++ Sources/Fetchers/NetworkFetcher.swift
@@ -10,14 +10,64 @@ import Foundation
 import UIKit
 import RxSwift
 
-class NetworkFetcher: Fetcher {
+public enum NetworkFetcherError: Error {
+    /// Used when the status code of the network response is not included in the range 200..<300
+    case statusCodeNotOk(response: HTTPURLResponse, data: Data?)
+    
+    /// Used when the network response had an invalid size
+    case invalidNetworkResponse(response: HTTPURLResponse)
+    
+    /// Used when the network request didn't manage to retrieve data
+    case noDataRetrieved(response: HTTPURLResponse)
+}
+
+public class NetworkFetcher: Fetcher {
     
     public typealias Key = URLRequest
     public typealias Value = NSData
     
     public lazy var session: URLSession = URLSession.shared
     
+    public init() {}
+    
+    private func validate(_ response: HTTPURLResponse, withData data: Data) -> Bool {
+        var responseIsValid = true
+        let expectedContentLength = response.expectedContentLength
+        if expectedContentLength > -1 {
+            responseIsValid = Int64(data.count) >= expectedContentLength
+        }
+        return responseIsValid
+    }
+    
     public func get(_ key: URLRequest) -> Observable<NSData?> {
-        return session.rx.data(request: key).map({ $0 as NSData })
+        return Observable.create {(o) -> Disposable in
+            
+            let task = self.session.dataTask(with: key, completionHandler: { (data, response, error) in
+                if let error = error {
+                    switch (error) {
+                    case URLError.cancelled:
+                        o.onCompleted()
+                    default:
+                        o.on(.error(error))
+                    }
+                } else if let httpResponse = response as? HTTPURLResponse {
+                    if !(200..<300).contains(httpResponse.statusCode) {
+                        o.on(.error(NetworkFetcherError.statusCodeNotOk(response: httpResponse, data: data)))
+                    } else if let data = data , !self.validate(httpResponse, withData: data) {
+                        o.on(.error(NetworkFetcherError.invalidNetworkResponse(response: httpResponse)))
+                    } else if let data = data {
+                        o.on(.next(data as NSData))
+                        o.onCompleted()
+                    } else {
+                        o.on(.error(NetworkFetcherError.noDataRetrieved(response: httpResponse)))
+                    }
+                }
+            })
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
 }
+
