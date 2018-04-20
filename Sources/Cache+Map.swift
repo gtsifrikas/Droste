9f2fc415f82e7fe15756e204608961e732fe75da
George Tsifrikas <tsifrikas@workable.com>
2018-04-20 17:44:51 +0300

    Introduce `flatMapValue` to allow for async transformation of caches.

--- Sources/Cache+Map.swift
+++ Sources/Cache+Map.swift
@@ -14,21 +14,27 @@ extension Cache {
     //this method allows to map the values from on cache using a transformer and allows to compose to virtually unrelated caches to a new cache, for example make a disk cache to know about jpeg images
     public func mapValues<V2>(f: @escaping (Value) throws -> V2, fInv: @escaping (V2) throws -> Value)
         -> CompositeCache<Key, V2> {
+            return flatMapValues(f: {.just(try f($0))}, fInv: {.just(try fInv($0))})
+    }
+    
+    public func flatMapValues<V2>(f: @escaping (Value) throws -> Observable<V2>, fInv: @escaping (V2) throws -> Observable<Value>)
+        -> CompositeCache<Key, V2> {
             return CompositeCache(
                 get: { (key) -> Observable<V2?> in
-                    return self.get(key).map { (originalValue: Value?) -> V2? in
+                    return self.get(key).flatMap { (originalValue: Value?) -> Observable<V2?> in
                         if let originalValue = originalValue {
-                            return try f(originalValue)
+                            return try f(originalValue).map { $0 }
                         }
-                        return nil
+                        return .just(nil)
                     }
             },
                 set: {value, key in
-                    return  Observable.just(value).map { try fInv($0) }.flatMap { self.set($0, for: key) }
+                    return  Observable.just(value).flatMap { try fInv($0) }.flatMap { self.set($0, for: key) }
             },
                 clear: clear
             )
     }
+    
     //this method allows to map the keys from on cache using a transformer and allows to compose to virtually unrelated caches to a new cache, for example transform NSURLRequest to String
     public func mapKeys<K2>(fInv: @escaping (K2) throws -> Key)
         -> CompositeCache<K2, Value> {
